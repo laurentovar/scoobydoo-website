336d909164cc0f46118332c94d233d2c03052f16
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-30 00:38:55 -0600

    added some functions for the pictures

--- scooby-game.js
+++ scooby-game.js
@@ -1,11 +1,23 @@
 //function calls the api and returns the response 
 
-function callAPI() {
+//2 = the default image
+let imageId = 2
+
+function getOptions(){
+
+}
+
+function getStoryText(){
+
+}
+
+function getImage(imageID) {
     // Create a request variable and assign a new XMLHttpRequest object to it.
     let request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'http://127.0.0.1:8080/api/v1/picture/2', true);
+
+    request.open('GET', 'http://127.0.0.1:8080/api/v1/picture/' + imageID, true);
 
     request.onload = function() {
         // Begin accessing JSON data here
@@ -20,20 +32,37 @@ function callAPI() {
     request.send()
 }
 
-callAPI();
+function nextPicture(){
+    imageId++
+    getImage(imageId);
+}
+
+getImage(imageId);
 
 //when you click the start button the card for the story text shows
 $("#startButton").click(function(){
     $("#story-text-body").show();
-});
 
-//when you click the start button the title hides
-$("#startButton").click(function(){
-    $(".glow").hide();
-});
-
-// when you click the start button the button hides
-$("#startButton").click(function(){
     $("#startButton").hide();
+    $(".glow").hide();
+
+    nextPicture()
+
+    $("#Option1").show();
+    $("#Option2").show();
 });
 
+
+$("#Option1").click(function () {
+    let x = $("#Option1").attr("name")
+    console.log(x);
+    getImage(x)
+
+})
+
+$("#Option2").click(function () {
+    let x = $("#Option2").attr("name")
+    console.log(x);
+    getImage(x)
+
+})
